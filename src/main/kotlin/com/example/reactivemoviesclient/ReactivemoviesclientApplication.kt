551468ebc752a29c47c61070a37c719479794d68
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-04-03 12:00:12 +0200

    RequestRateLimiter not compiling

--- src/main/kotlin/com/example/reactivemoviesclient/ReactivemoviesclientApplication.kt
+++ src/main/kotlin/com/example/reactivemoviesclient/ReactivemoviesclientApplication.kt
@@ -3,7 +3,10 @@ package com.example.reactivemoviesclient
 import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.support.beans
 import org.springframework.web.reactive.function.client.WebClient
@@ -59,6 +62,18 @@ fun main(args: Array<String>) {
                                     //works with Eureka
                                     uri("http://localhost:8081/movies")
                                 }
+
+//                                route {
+//                                    val rl = ref<RequestRateLimiterGatewayFilterFactory>()
+//                                    val redisRl = RedisRateLimiter(5,10)
+//                                    val filter = rl.apply(redisRl.config)
+//
+//                                    path("/rl")
+//                                    filters {
+//                                        filter(rl)
+//                                    }
+//                                    uri("http://localhost:8081/movies")
+//                                }
 
                             }
                         }
