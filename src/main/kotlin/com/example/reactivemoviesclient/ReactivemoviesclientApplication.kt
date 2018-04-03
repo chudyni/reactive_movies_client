ec5a4e4a39740d30597fe9257990a4f9af3049e4
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-04-03 11:04:49 +0200

    Init commit

---
+++ src/main/kotlin/com/example/reactivemoviesclient/ReactivemoviesclientApplication.kt
@@ -0,0 +1,13 @@
+package com.example.reactivemoviesclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactivemoviesclientApplication
+
+fun main(args: Array<String>) {
+
+    //function bean definitions
+
+}
